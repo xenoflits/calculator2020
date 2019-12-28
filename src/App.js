eda4dab670dd212016debc2781fc4bc39e212f77
unknown <soeradj.mahabiersing@gmail.com>
2019-12-28 22:11:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Button from './components/button';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      buttons: [
+        ["pi","Pi"],
+        ["equil","="],
+        ["times","times"],
+        ["divide","/"],
+        ["minus","-"],
+        ["clear","C"],
+        ["point","."],
+        ["plus","+"],
+        ["one","1"],
+        ["two","2"],
+        ["three","3"],
+        ["four","4"],
+        ["five","5"],
+        ["six","6"],
+        ["seven","7"],
+        ["eight","8"],
+        ["nine","9"],
+        ["zero","0"]
+      ]
+    }
+    
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+      <div className="Display">display
+
+      </div>
+      <div className="Brand"></div>
+      <div className="Button-pad">
+        
     </div>
-  );
+    </div>
+    )
+  }
+  
 }
+
+
 
 export default App;
---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) => {
+    return (
+        <div className={this.props.name}>{this.props.value}</div>
+    )
+
+}
+
+export default Button
